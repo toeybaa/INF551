b02e0341d94e28d1fb7e8ad853ea258a2c77f79d
Patcharapon Nuimark <peco@patcharaponsmbp.attlocal.net>
2020-02-19 01:56:03 -0800

    argument fixed

--- test.py
+++ test.py
@@ -4,24 +4,23 @@ import json
 
 #Get Command Line Arguments
 def main(argv):
-    input_file = ''
-    output_file = ''
-    format = ''
+    input_file = argv[0];
+    output_file = argv[1];
+    print (input_file);
+    print (output_file);
     try:
         opts, args = getopt.getopt(argv,"hi:o:f:",["ifile=","ofile=","format="])
     except getopt.GetoptError:
         print ('csv_json.py -i <path to inputfile> -o <path to outputfile> -f <dump/pretty>')
         sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print ('csv_json.py -i <path to inputfile> -o <path to outputfile> -f <dump/pretty>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            input_file = arg
-        elif opt in ("-o", "--ofile"):
-            output_file = arg
-        elif opt in ("-f", "--format"):
-            format = arg
+    # for opt, arg in opts:
+    #     if opt == '-h':
+    #         print ('csv_json.py -i <path to inputfile> -o <path to outputfile> -f <dump/pretty>')
+    #         sys.exit()
+    #     elif opt in ("-i", "--ifile"):
+    #         input_file = arg
+    #     elif opt in ("-o", "--ofile"):
+    #         output_file = arg
     read_csv(input_file, output_file, format)
 
 #Read CSV File
@@ -37,10 +36,7 @@ def read_csv(file, json_file, format):
 #Convert csv data into json and write it
 def write_json(data, json_file, format):
     with open(json_file, "w") as f:
-        if format == "pretty":
-            f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': '), ensure_ascii=False))
-        else:
-            f.write(json.dumps(data))
+        f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': '), ensure_ascii=False))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
